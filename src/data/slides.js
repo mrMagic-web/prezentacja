46508e6b9ba160773e1784bedf62ea557fdf327a
Maciej Łukasz D <maciejdrahusz@gmail.com>
2018-11-02 11:04:51 +0100

    New Slides

--- src/data/slides.js
+++ src/data/slides.js
@@ -7,16 +7,44 @@ const slides = [
     icon: "grid layout"
   },
   {
-    title: "Pierwszy slajd",
-    subtitle: "Tekst pierwszegfo slajdu",
+    title: "Cym jest CSS?",
+    subtitle: "Cascading Style Sheets - Kaskadowe arkusze stylów",
+    tekst: "Nazwa „kaskadowe arkusze stylów” wynika z faktu, iż gdy reguły CSS wykluczają się wzajemnie w arkuszu zewnętrznym, arkuszu wewnętrznym oraz na poziomie elementów HTML, priorytet stylów ustalany jest hierarchicznie. Przyjęto, że oddziaływanie stylów z arkuszy zewnętrznych może być modyfikowane przez style zdefiniowane w nagłówku dokumentu, te zaś mogą być modyfikowane przez reguły zdefiniowane bezpośrednio w ciele dokumentu. Pierwszeństwo mają zatem style zdefiniowane „bliżej” formatowanego elementu."
+    list: ""
     background: "style-2.jpg",
-    layout: "photo-left"
+    layout: "photo-left",
+    icon: "question circle outline"
   },
   {
-    title: "Drugi slajd",
-    subtitle: "Tekst drugiego slajdu",
+    title: "CSS w akcji",
+    subtitle: "Układ, zdjęcia, tekst, pozycja, kolory i wiele innych",
+    link: {
+      text: "CSS Zen Garden, jedna strona w ponad 200 wersjach graficznych",
+      url: "http://www.csszengarden.com/221/"
+    }, 
     background: "style-3.jpg",
     layout: "photo-right"
-  }
+  }, 
+  {
+    title: "Składnia arkuszy",
+    subtitle: "Reguła składa się z selektora oraz deklaracji. Selektor określa element lub grupę elementów, którego ma dotyczyć deklaracja.",
+    link: {
+      text: "CSS Zen Garden, jedna strona w ponad 200 wersjach graficznych",
+      url: "http://www.csszengarden.com/221/"
+    },
+    kod: [ `selektor { 
+      właściwość: wartość 
+    }`,
+    `selektor1, selektor2 { 
+      właściwość1: wartość1; 
+      właściwość2: wartość2; 
+    }`, 
+    `h1 { 
+      color: blue; 
+      font-size: 24px;
+    }` ],
+    background: "style-3.jpg",
+    layout: "photo-right"
+  },
 ];
 export default slides;
