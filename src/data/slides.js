b5bd827cf0d2307460f01fcd4eb7c9c5ff287805
mrMagic-web <maciejdrahusz@gmail.com>
2018-11-22 23:07:27 +0100

    New Slides

--- src/data/slides.js
+++ src/data/slides.js
@@ -11,22 +11,22 @@ const slides = [
     background: "winner.jpg",
     layout: "photo-full"
   },
-    {
+  {
     title: "Z czego składa się strona internetowa?",
 
     list: [
       "HTML - treść strony(zdjęcia, tekst, linki, forma kontaktowa)",
       "CSS - warstwa wizualna strony internetowej(kolory, rozmiary, układ strony)",
       "Java Script - frontend - logika i interakcje (jak tu kliknę co się stanie?)",
-	  "Java Script - backend - Obsługa serwera(Node.js)",
+      "Java Script - backend - Obsługa serwera(Node.js)"
     ],
     background: "animate-basic.gif",
     layout: "photo-right"
   },
   {
     title: "Czym jest JavaScript",
-    subtitle: "Język skryptowy używany do obsługi strony w przeglądarce (front end) i po stronie serwerów (front end) na platformie Node",
-	text: "żywamy",
+    subtitle:
+      "Język skryptowy używany do obsługi strony w przeglądarce (front end) i po stronie serwerów (front end) na platformie Node",
     list: [
       "zwraca uwagę na element",
       "podkreśla istotną zmianę na stronie",
@@ -38,5 +38,19 @@ const slides = [
     background: "animate-basic.gif",
     layout: "photo-right"
   },
+  {
+    title: "Chrome Developer Tools",
+    subtitle: "Nieodłączne narzędzie developera Java Script.",
+    list: [
+      "Console - Wyświetla błędy, logi i pozwala na debugowanie i szybkie testowanie kodu.",
+      "Sources - pozwala na obejrzenie plików i bezpośrednie ich edytowanie",
+      "Network - pozwala na monitorowanie zapytań i odpowiedzi do backendu",
+      "Performance - pozwala na zmierzenie procesów",
+      "Application - informacje o danych używanych przez stronę",
+      
+    ],
+    background: "animate-basic.gif",
+    layout: "photo-right"
+  }
 ];
 export default slides;
