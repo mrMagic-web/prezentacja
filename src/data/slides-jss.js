54c4d15e6d96f48d03906aba4ddbe70a00fa82bc
Maciej.Drahusz <maciej.drahusz@partners.mbank.pl>
2018-11-22 08:53:45 +0100

    Some basic changes

---
+++ src/data/slides-jss.js
@@ -0,0 +1,42 @@
+const slides = [
+  {
+    title: "Java Script Intro",
+    subtitle: "yeah bebe",
+    background: "homer.gif",
+    layout: "photo-full"
+  },
+  {
+    title: "Winner",
+    subtitle: "?",
+    background: "winner.jpg",
+    layout: "photo-full"
+  },
+    {
+    title: "Z czego składa się strona internetowa?",
+
+    list: [
+      "HTML - treść strony(zdjęcia, tekst, linki, forma kontaktowa)",
+      "CSS - warstwa wizualna strony internetowej(kolory, rozmiary, układ strony)",
+      "Java Script - frontend - logika i interakcje (jak tu kliknę co się stanie?)",
+	  "Java Script - backend - Obsługa serwera(Node.js)",
+    ],
+    background: "animate-basic.gif",
+    layout: "photo-right"
+  },
+  {
+    title: "Czym jest JavaScript",
+    subtitle: "Język skryptowy używany do obsługi strony w przeglądarce (front end) i po stronie serwerów (front end) na platformie Node",
+	text: "żywamy",
+    list: [
+      "zwraca uwagę na element",
+      "podkreśla istotną zmianę na stronie",
+      "istotny element dobrego UX-u",
+      "pozwala na pokazanie, że coś zajmie czas (loading...)",
+      "zwiększa efekt wow waszych stron",
+      "co jeszcze?"
+    ],
+    background: "animate-basic.gif",
+    layout: "photo-right"
+  },
+];
+export default slides;
